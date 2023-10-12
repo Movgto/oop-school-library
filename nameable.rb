d9880c088a9f894dc670cd19b5e97fa987749af6
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-10-12 10:05:33 -0600

    Add error message in 'correct_name' method for Nameable

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,8 @@
 class Nameable
   def correct_name
-    raise NotImplementedError
+    raise NotImplementedError.new("Method '#{__method__.to_s}' is not implemented for class '#{self.class}'")
   end
 end
+
+nameable = Nameable.new
+nameable.correct_name
