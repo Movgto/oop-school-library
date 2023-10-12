7e1cf9ff993ca0d8ce357e65d01fb92c5ee71992
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-10-12 16:14:51 -0600

    Add Classroom class and associations

--- person.rb
+++ person.rb
@@ -1,16 +1,16 @@
 require_relative 'nameable'
-require_relative 'decorators'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
     @id = object_id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -27,10 +27,3 @@ class Person < Nameable
 
   private :of_age?
 end
-
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
