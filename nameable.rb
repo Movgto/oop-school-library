6c183b5c6c09f3e151998886a74d89c91477acb2
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-10-12 10:07:23 -0600

    Fix linters

--- nameable.rb
+++ nameable.rb
@@ -1,6 +1,6 @@
 class Nameable
   def correct_name
-    raise NotImplementedError.new("Method '#{__method__.to_s}' is not implemented for class '#{self.class}'")
+    raise NotImplementedError, "Method '#{__method__}' is not implemented for class '#{self.class}'"
   end
 end
 
