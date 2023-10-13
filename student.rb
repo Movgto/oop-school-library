2fbc5a221ee460e85e2922b5de5b2b0bea32133a
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-10-12 19:07:43 -0600

    Rebase to dev

--- student.rb
+++ student.rb
@@ -7,9 +7,26 @@ class Student < Person
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+    @classroom = classroom
+    classroom.add_student(self)
+  end
+
+  def assign_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
 end
+
+classroom = Classroom.new(301)
+student = Student.new(13, classroom, 'Mario')
+book_dune = Book.new('Dune', 'Frank Herbert')
+rental = Rental.new('2023-10-21', book_dune, student)
+
+p student.rentals[0].date
+p book_dune.rentals[0].date
+p rental.book.author
+p rental.person.name
