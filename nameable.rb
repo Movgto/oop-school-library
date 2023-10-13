fa09e2dae6c9a0f3a7607447a5cdb41dea8d6de6
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-10-12 18:55:51 -0600

    Revert "Fix linters"
    This reverts commit 6c183b5c6c09f3e151998886a74d89c91477acb2.

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,11 @@
 class Nameable
   def correct_name
-    raise NotImplementedError, "Method '#{__method__}' is not implemented for class '#{self.class}'"
+    raise NotImplementedError.new("Method '#{__method__.to_s}' is not implemented for class '#{self.class}'")
   end
 end
+<<<<<<< HEAD
+=======
+
+nameable = Nameable.new
+nameable.correct_name
+>>>>>>> parent of 6c183b5 (Fix linters)
