b98f92e20ea419394c310434bf16c615e869bdea
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-19 07:24:30 +0430

    linter fixed

--- app.rb
+++ app.rb
@@ -9,11 +9,10 @@ module Library
   end
 
   def valid_number?(range, choice)
-    unless range.include?(choice)
-      wrong_number_msg
-      return false
-    end
-    true
+    return true if range.include?(choice)
+
+    wrong_number_msg
+    false
   end
 
   def success_msg(label)
@@ -22,48 +21,44 @@ module Library
 
   def get_list_books(data, show_index = false)
     puts "\nAll Books:"
-    if data[:books].any?
-      data[:books].each.with_index do |book, index|
-        book_index = show_index ? "#{index})" : ''
-        puts "#{book_index} Title: #{book.title}, Author: #{book.author}"
-      end
-    else
-      puts 'No record found'
-    end
+    data[:books].any? ? display_books(data[:books], show_index) : puts('No record found')
     puts "\n"
   end
 
-  def get_list_person(data, show_index = false)
+  def get_list_people(data, show_index = false)
     puts "\nAll People:"
-    if data[:people].any?
-      data[:people].each.with_index do |people, index|
-        person_index = show_index ? "#{index})" : ''
-        puts "#{person_index} #{people.description}"
-      end
-    else
-      puts 'No record found'
+    data[:people].any? ? display_people(data[:people], show_index) : puts('No record found')
+    puts "\n"
+  end
+
+  def display_books(books, show_index)
+    books.each.with_index do |book, index|
+      book_index = show_index ? "#{index})" : ''
+      puts "#{book_index} Title: #{book.title}, Author: #{book.author}"
     end
-    puts "\n"
   end
 
-  def get_user_rental(data)
-    puts 'All rentals for a given person number (not id)'
-    get_list_person(data, true)
-    idx = gets.chomp
-    data[:people][idx.to_i].rentals_description
-    puts "\n"
+  def display_people(people, show_index)
+    people.each.with_index do |person, index|
+      person_index = show_index ? "#{index})" : ''
+      puts "#{person_index} #{person.description}"
+    end
   end
 
-  def add_new_person(*)
+  def select_person(data)
+    puts 'Select a person from the following list by number (not id)'
+    get_list_people(data, true)
+    gets.chomp.to_i
+  end
+
+  def add_new_person
     puts 'Do you want to create a student (1) or a teacher (2) [Input the number]:'
     choice = gets.chomp
     return unless valid_number?(%w[1 2], choice)
-
     puts 'Age:'
     age = gets.chomp
     puts 'Name:'
     name = gets.chomp
-
     case choice
     when '1'
       create_student(age, name)
@@ -90,7 +85,7 @@ module Library
     success_msg('Person')
   end
 
-  def add_new_book(*)
+  def add_new_book
     book = Book.new(nil, nil)
     puts 'Title:'
     book.title = gets.chomp
@@ -104,12 +99,10 @@ module Library
     puts 'Select a book from the following list by number'
     get_list_books(data, true)
     book_index = gets.chomp
-    puts 'Select a person from the following list by number (not id)'
-    get_list_person(data, true)
-    person_index = gets.chomp
+    person_index = select_person(data)
 
-    if !data[:people][person_index.to_i].nil? && !data[:books][book_index.to_i].nil?
-      rental = Rental.new(data[:people][person_index.to_i], data[:books][book_index.to_i], nil)
+    if data[:people][person_index] && data[:books][book_index]
+      rental = Rental.new(data[:people][person_index], data[:books][book_index], nil)
       puts 'Date:'
       rental_date = gets.chomp
       rental.date = rental_date
